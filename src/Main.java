0938d8074a0b389bd78fce4ebdfa531fdc39cc19
Jim Rennie <12226713+Jimeh87@users.noreply.github.com>
2014-05-03 07:50:38 +0000

    Creation of City and updated Grid with new function

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import objects.Grid;
+import objects.Tile;
+
 
 
 public class Main {
 
-	public static void main(String[] args) {
-		InitGame.launchApplication(args);
+	private static boolean running;
+	private static City city = new City(45, 45);
 
+	public static void main(String[] args) {
+		initializeGame(args);
+		//run();
 	}
+
+	private static void run() {
+
+		long lastTime = System.nanoTime();
+		final double amountOfTicks = 60D;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			List<Tile> tilesToUpdate;
+			if (delta >= 1) {
+				tilesToUpdate = tick();
+				delta--;
+			}
+			render();
+		}
+	}
+
+	private static ArrayList<Tile> tick() {
+		city.processTick();
+		
+		return null;
+		
+	}
+
+	private static void render() {
+		
+	}
+
+	private static void initializeGame(String[] args) {
+		InitGame.launchApplication(args);
+	}
+	
+	
+	
 
 }
