ff3531e82d2252fcf804016b42259ba3f2c89584
Jim Rennie <12226713+Jimeh87@users.noreply.github.com>
2014-05-03 04:57:48 +0000

    Update to Grid

--- src/objects/Grid.java
+++ src/objects/Grid.java
@@ -1,13 +1,37 @@
 package objects;
 
 public class Grid {
-	Grid(int x, int y) {
-		this.x = x;
-		this.y = y;
+	Grid(int xSize, int ySize) {
+		this.xSize = xSize;
+		this.ySize = ySize;
+		this.tileArray = initializeGrid();
 	}
 	
-	int x;
-	int y;
+	int xSize;
+	int ySize;
+	
+	Tile[][] tileArray;
+	
+	private Tile[][] initializeGrid() {
+		tileArray = new Tile[xSize][ySize];
+		
+		for (int x = 0; x < xSize; x++) {
+			for (int y = 0; y < ySize; y++) {
+				tileArray[x][y] = new Tile(x, y, TileType.EMPTY);
+			}
+		}
+		
+		return tileArray;
+	}
+	
+	public Tile getTile(int xLocation, int yLocation) {
+		return tileArray[xLocation][yLocation];
+	}
+
+	public void setTile(int xLocation, int yLocation, Tile tile) {
+		tileArray[xLocation][yLocation] = tile;
+	}
+	
 	
 	
 }
