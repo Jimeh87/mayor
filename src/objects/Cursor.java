1b9ca34bcad7815db7250864f10f48fce5fe9b4e
JoshSinclair <JoshSinclair@406cba41-ab39-4095-9139-72de8d0b1667>
2014-05-04 21:13:07 +0000

    cursor start

---
+++ src/objects/Cursor.java
@@ -0,0 +1,38 @@
+package objects;
+
+
+public class Cursor{
+	
+	//Fields
+	String cursorType;
+	Integer xLocation;
+	Integer yLocation;
+	
+	//Construct
+	public Cursor(String type, Integer xLocation, Integer yLocation) {
+		this.cursorType = type;
+		this.xLocation = xLocation;
+		this.yLocation = yLocation;
+	}
+	
+	//Methods
+	public String getCursorType() {
+		return cursorType;
+	}
+	public void setCursorType(String type) {	
+		this.cursorType = type;
+	}
+	public Integer getXLocation(){
+		return xLocation;
+	}
+	public void setXLocation(Integer xLocation){
+		this.xLocation = xLocation;
+	}
+	public Integer getYLocation(){
+		return yLocation;
+	}
+	public void setYLocation(Integer yLocation){
+		this.yLocation = yLocation;
+	}
+	
+}
