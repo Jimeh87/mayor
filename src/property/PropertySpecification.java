71b8a3b6f6e9ed6f02c0c7cdd3bb53875e56852c
mayor <mayor@406cba41-ab39-4095-9139-72de8d0b1667>
2014-06-22 23:50:12 +0000

    removing everything... in hopes of fixing reorg. hope it works...

--- src/property/PropertySpecification.java
+++
@@ -1,8 +0,0 @@
-package property;
-
-import objects.Specification;
-
-public abstract class PropertySpecification extends Specification {
-	public abstract void remove();
-	public abstract void tick();
-}
