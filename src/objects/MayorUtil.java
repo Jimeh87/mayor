18a834b6381fb4991ddf8b9ba273d910c83bb0b5
mayor <mayor@406cba41-ab39-4095-9139-72de8d0b1667>
2014-06-24 05:17:49 +0000

    put comment in proper place for MayorUtil

--- src/objects/MayorUtil.java
+++ src/objects/MayorUtil.java
@@ -5,11 +5,16 @@ import java.util.Random;
 
 /**
  * @author Jim
- * Get random enum from enum class passed in
+ * Main util service
  */
 public class MayorUtil {
 	private static final Random random = new SecureRandom();
 	
+    /**
+     * Get random enum from enum class passed in
+     * @param clazz
+     * @return
+     */
     public static <T extends Enum<?>> T randomEnum(Class<T> clazz){	
         int x = random.nextInt(clazz.getEnumConstants().length);
         return clazz.getEnumConstants()[x];
